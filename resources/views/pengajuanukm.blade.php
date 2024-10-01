5e4c54b671c63fe22c55b7d8ca450222f64df06d
syahlasals <syahlasbl28@gmail.com>
2024-10-01 14:04:59 +0700

    feat: add navbar

--- resources/views/pengajuanukm.blade.php
+++ resources/views/pengajuanukm.blade.php
@@ -12,6 +12,9 @@
 <script src="../path/to/flowbite/dist/flowbite.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.js"></script>
 <body>
+    @include('components.navbar')
+    @include('components.sidebar')
+
     <section class="bg-blue dark:bg-gray-900">
         <div class="py-8 px-4 mx-auto max-w-4xl lg:py-16">
             <h2 class="mb-4 text-xl font-bold text-blue-800 dark:text-white">Pengajuan Ketua Himpunan</h2>
@@ -39,7 +42,7 @@
                             <option value="Teknik Sipil">Teknik Sipil</option>
                         </select>
                     </div>
-    
+
                     <!-- Program Studi -->
                     <div>
                         <label for="Prodi" class="block mb-2 text-sm font-medium text-blue-800 dark:text-white">Program Studi</label>
@@ -49,7 +52,7 @@
                             <option value="D4 Informatika">D4 Informatika</option>
                         </select>
                     </div>
-    
+
                     <!-- Organisasi Mahasiswa -->
                     <div>
                         <label for="Ormawa" class="block mb-2 text-sm font-medium text-blue-800 dark:text-white">Ormawa</label>
@@ -59,7 +62,7 @@
                             <option value="HIMATEL">HIMATEL</option>
                         </select>
                     </div>
-    
+
                     <!-- Periode -->
                     <div>
                         <label for="periode" class="block mb-2 text-sm font-medium text-blue-800 dark:text-white">Periode</label>
@@ -70,20 +73,20 @@
                             <option value="2022">2022-2023</option>
                         </select>
                     </div>
-    
+
                     <!-- No.Telepon -->
                     <div>
                         <label for="notelepon" class="block mb-2 text-sm font-medium text-blue-800 dark:text-white">No.Telepon</label>
                         <input type="text" name="notelepon" id="notelepon" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Nomor Telepon Anda" required="">
                     </div>
-    
+
                     <!-- Email -->
                     <div>
                         <label for="email" class="block mb-2 text-sm font-medium text-blue-800 dark:text-white">Email</label>
                         <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Email Anda" required="">
                     </div>
                 </div>
-    
+
                 <div class="flex justify-end mt-4">
                     <button type="submit" class="inline-flex items-center px-5 py-2.5 text-sm font-medium text-white bg-blue-800 border border-blue-800 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-gray-100">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 mr-2 text-blue-800">
@@ -92,11 +95,11 @@
                         Next
                     </button>
                 </div>
-                
+
             </form>
         </div>
     </section>
-    
+
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
